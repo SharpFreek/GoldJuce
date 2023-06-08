5f6ddbd367b1e619ee0eadb972e38108682204a7
GeoffStengel <geoffsglass@hotmail.com>
2023-06-08 11:25:18 +0200

    Add files via upload
    Cleaning up code a bit so it doesn't get to sloppy, noticed even the push messages had cut off the Add Files via Upload part. not sure if already pushed this one, but still been doing design and such

--- js/gsap.js
+++ js/gsap.js
@@ -1,3 +1,3 @@
-gsap.from('.img-description', {duration: 1, y: '-100%', ease : 'elastic'});
+gsap.from('.img-description', {duration: 3, y: '20vw', ease : 'elastic'});
 
-gsap.from('.img-description', { duration: 1, opacity: 0, delay: .5, stagger: .5 });
+gsap.from('.img-description', { duration: 1, opacity: 0.5, delay: .5, stagger: .5 });
